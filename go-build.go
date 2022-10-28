40991e686a267d679938bd1c8863cdb7167b5c60
Robert Pająk <pellared@hotmail.com>
2022-10-28 19:06:16 +0200

    Trim extension in go-build

--- go-build.go
+++ go-build.go
@@ -2,6 +2,7 @@ package workflow
 
 import (
 	"os/exec"
+	"strings"
 
 	"github.com/goyek/goyek/v2"
 )
@@ -10,10 +11,11 @@ var TaskGoBuild = goyek.Define(goyek.Task{
 	Name:  "go-build",
 	Usage: "go build",
 	Action: func(tf *goyek.TF) {
-		ext, err := exec.CommandContext(tf.Context(), "go", "env", "GOEXE").CombinedOutput()
+		extBytes, err := exec.CommandContext(tf.Context(), "go", "env", "GOEXE").CombinedOutput()
 		if err != nil {
 			tf.Fatal(err)
 		}
-		Exec(tf, `go build -ldflags="-s -w" -o dist/app`+string(ext))
+		ext := strings.TrimSpace(string(extBytes))
+		Exec(tf, `go build -ldflags="-s -w" -o dist/app`+ext)
 	},
 })
